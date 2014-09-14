e0cf6ad2917f2e292844e6e8054b4dd1ad10bb30
firstblood1985 <limin.hust@foxmail.com>
2014-09-14 09:36:35 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,28 +1,38 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+subject { page }
   describe "Home page" do
-
-    it "should have the content 'iTrack'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('iTrack')
-      expect(page).to have_title('iTrack | Home')
+	before {visit root_path}
+	it do
+	should have_content('iTrack') 
+	should have_title(full_title(''))
     end
   end
 
   describe "Help Page" do
+	before {visit help_path}
+
   	it "should have the content 'Help' "do
-  	visit '/static_pages/help'
+  	#visit '/static_pages/help'
   	expect(page).to have_content('Help')
     expect(page).to have_title('iTrack | Help')
   end
   end
 
   describe "About Page" do
+	before {visit about_path}
   	it "should have the content 'About me'" do
-  		visit '/static_pages/about'
   		expect(page).to have_content('About me')
 	    expect(page).to have_title('iTrack | About me')
+  	end
+  end
+
+  describe "Contact Page" do
+	before {visit contact_path}
+  	it "should have content 'Contact'" do 
+  		expect(page).to have_content('Contact')
+	    expect(page).to have_title('iTrack | Contact')
   	end
   end
 end
